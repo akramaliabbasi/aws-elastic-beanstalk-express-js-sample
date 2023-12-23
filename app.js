6846a265912bc918f785be449cc620e7b6f1915b
Akram Abbasi <mohdakramabbasi@gmail.com>
2023-12-23 17:14:35 +0400

    Added just response text3

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World This is new change from DevOps Engineer'));
-app.get('/', (req, res) => res.send('and Welcome here!'));
+app.get('/', (req, res) => res.send('Hello World This is new change from DevOps Engineer<Br>and Welcome here!'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
